b31614014d12ec07b7a98a788a097adffaef45d5
Bowen7 <zwxdyx@foxmail.com>
2026-01-13 23:22:16 +0800

    upgrade deps and replace f64 with f32

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,5 @@
 //! This module provides a conceptual structure of points and half-lines.
-//! 
+//!
 //! See more details on each item.
 
 mod coordinate;
@@ -8,33 +8,41 @@ mod ray;
 pub use coordinate::Coordinate;
 pub use ray::Ray;
 
-const EPS: f64 = 1e-9;
+const EPS: f32 = 1e-6;
 
-pub(crate) fn feq(x: f64, y: f64) -> bool{
-    f64::abs(x-y) < EPS
+pub(crate) fn feq(x: f32, y: f32) -> bool {
+    f32::abs(x - y) < EPS
 }
 
-pub(crate) fn fneq(x: f64, y: f64) -> bool{
+pub(crate) fn fneq(x: f32, y: f32) -> bool {
     !feq(x, y)
 }
 
-pub(crate) fn fgt(x: f64, y: f64) -> bool{
-    if feq(x, y) {return false;}
+pub(crate) fn fgt(x: f32, y: f32) -> bool {
+    if feq(x, y) {
+        return false;
+    }
     x > y
 }
 
 #[allow(dead_code)]
-pub(crate) fn flt(x: f64, y: f64) -> bool{
-    if feq(x, y) {return false;}
+pub(crate) fn flt(x: f32, y: f32) -> bool {
+    if feq(x, y) {
+        return false;
+    }
     x < y
 }
 
-pub(crate) fn fgeq(x: f64, y: f64) -> bool{
-    if feq(x, y) {return true;}
+pub(crate) fn fgeq(x: f32, y: f32) -> bool {
+    if feq(x, y) {
+        return true;
+    }
     x > y
 }
 
-pub(crate) fn fleq(x: f64, y: f64) -> bool{
-    if feq(x, y) {return true;}
+pub(crate) fn fleq(x: f32, y: f32) -> bool {
+    if feq(x, y) {
+        return true;
+    }
     x < y
 }
